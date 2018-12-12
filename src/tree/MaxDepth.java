10cb8e3c935ee9885ff464d1e7b8cfd32a7ae08b
BaoZhou <107630221@qq.comgit>
2018-12-12 17:42:56 +0800

    finish tree

---
+++ src/tree/MaxDepth.java
@@ -0,0 +1,26 @@
+package tree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author BaoZhou
+ * @date 2018/12/12
+ */
+public class MaxDepth {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            TreeNode root = Wrapper.stringToTreeNode(line);
+            Wrapper.prettyPrintTree(root);
+            System.out.println(maxDepth(root));
+        }
+    }
+
+    public static int maxDepth(TreeNode root) {
+        return root == null ? 0 : (1 + Math.max(maxDepth(root.left), maxDepth(root.right)));
+    }
+
+}
