1e76d13929ed0bd675cd893aae7399b3e03cc992
BaoZhou <107630221@qq.comgit>
2019-01-03 11:13:01 +0800

    【leetcode】二叉搜索树中第K小的元素 【leetcode】岛屿的个数

--- src/main/java/com/leetcode/middle/tree/KthSmallest.java
+++ src/main/java/com/leetcode/middle/tree/KthSmallest.java
@@ -2,6 +2,7 @@ package com.leetcode.middle.tree;
 
 import com.leetcode.leetcodeutils.TreeNode;
 import com.leetcode.leetcodeutils.TreeWrapper;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,20 +12,22 @@ import java.util.List;
  * @date 2019/1/3
  */
 public class KthSmallest {
-    public static void main(String[] args) {
+    @Test
+    public void result()
+    {
         TreeNode root = TreeWrapper.stringToTreeNode("[4,2,5,1,3]");
         TreeWrapper.prettyPrintTree(root);
         System.out.println(kthSmallest(root, 5));
     }
 
-    public static int kthSmallest(TreeNode root, int k) {
+    public int kthSmallest(TreeNode root, int k) {
         dfs(root);
         return list.get(k - 1);
     }
 
-    static List<Integer> list = new ArrayList();
+    List<Integer> list = new ArrayList();
 
-    public static void dfs(TreeNode root) {
+    public void dfs(TreeNode root) {
         if (root == null) {
             return;
         }
